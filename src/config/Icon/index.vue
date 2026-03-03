7d918243c2cff20e2852f100683ef071b1f11ec5
sj-cai <2228682173@qq.com>
2026-03-03 17:10:01 +0800

    feat:增加icon页面

---
+++ src/config/Icon/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <span class="iconfont"></span>
+</template>
+
+<script>
+export default {
+    name: 'BaseIcon'
+}
+</script>
+
+<style scoped lang="scss">
+.iconfont {
+    display: inline-block;
+    text-align: center;
+    width: 24px;
+    margin-right: 5px;
+    font-size: 16px;
+    color: #606266;
+}
+</style>
